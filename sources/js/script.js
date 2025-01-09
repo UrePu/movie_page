89a8f4e5adfdd08a61a20127b06a1122b3cef7bf
UrePu <darkin31313@gmail.com>
2025-01-09 12:21:29 +0900

    getDataFromApi

---
+++ sources/js/script.js
@@ -0,0 +1,26 @@
+import getData from "./tmdbApi.js";
+const imgUrlMain = "https://image.tmdb.org/t/p/w500";
+let getDataFromApi = async (urlDetail, urlNum, word) => {
+  if (!urlDetail) {
+    urlDetail = "top_rated";
+    // console.log("A");
+  }
+  if (!urlNum) {
+    // console.log("A");
+
+    urlNum = 1;
+  }
+  let url;
+  if (!word) {
+    url = `https://api.themoviedb.org/3/movie/${urlDetail}?language=ko-KR&page=${urlNum}`;
+  } else {
+    let encodedText = encodeURIComponent(word);
+    let adult = true;
+    console.log(encodedText);
+
+    url = `https://api.themoviedb.org/3/search/movie?query=${encodedText}&include_adult=${adult}&language=ko-KR&page=${urlNum}`;
+  }
+  let data = await getData(url);
+
+  pageConstruct(data);
+};
