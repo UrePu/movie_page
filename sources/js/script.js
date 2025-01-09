823d8ccc473f7a1e4e5e4a12dd9aadf3212cd47c
UrePu <darkin31313@gmail.com>
2025-01-09 20:01:07 +0900

    search

--- sources/js/script.js
+++ sources/js/script.js
@@ -1,6 +1,6 @@
 import getData from "./tmdbApi.js";
 const imgUrlMain = "https://image.tmdb.org/t/p/w500";
-let getDataFromApi = async (urlDetail, urlNum, word) => {
+let getDataFromApi = async (urlDetail, urlNum, word, clearPage) => {
   if (!urlDetail) {
     urlDetail = "top_rated";
     // console.log("A");
@@ -10,26 +10,35 @@ let getDataFromApi = async (urlDetail, urlNum, word) => {
 
     urlNum = 1;
   }
+  if (!clearPage) {
+    clearPage = false;
+  }
   let url;
   if (!word) {
     url = `https://api.themoviedb.org/3/movie/${urlDetail}?language=ko-KR&page=${urlNum}`;
   } else {
     let encodedText = encodeURIComponent(word);
-    let adult = true;
-    console.log(encodedText);
+    let adult = false;
+    // console.log(encodedText);
 
     url = `https://api.themoviedb.org/3/search/movie?query=${encodedText}&include_adult=${adult}&language=ko-KR&page=${urlNum}`;
   }
   let data = await getData(url);
+  console.log(data);
 
-  pageConstruct(data);
+  pageConstruct(data, clearPage);
 };
 
-let pageConstruct = async (data) => {
+let pageConstruct = async (data, clearPage) => {
+  const contentContainer = document.querySelector(".content_container");
+  if (clearPage) {
+    while (contentContainer.firstChild) {
+      contentContainer.removeChild(contentContainer.firstChild);
+    }
+  }
   let htmlData = data.results.map((d) => {
     return [d.id, d.poster_path];
   });
-  const contentContainer = document.querySelector(".content_container");
   htmlData.forEach((e) => {
     const newContent = document.createElement("div");
     newContent.className = "content";
@@ -55,4 +64,38 @@ let moreData = (totalPage) => {
 moreData();
 getDataFromApi(null, 1, null);
 
-let searchData = () => {};
+let searchData = () => {
+  let word = document.querySelector(".main_text");
+  document.querySelector(".header_search").addEventListener(
+    "input",
+    debounce((e) => {
+      getDataFromApi(null, 1, e.target.value, true);
+      word.innerHTML = "검색 결과";
+    }, 1000)
+  );
+};
+
+// let scrollDown = () => {
+//   window.addEventListener(
+//     "scroll",
+//     debounce((e) => {
+//       moreData();
+//     }, 1000)
+//   );
+// };
+function debounce(func, delay) {
+  // timeoutID를 할당할 변수를 선언합니다.
+  let timer = null;
+
+  // timer 변수에 접근 가능한 클로저 함수를 반환합니다.
+  return function (...args) {
+    // 만약 타이머가 존재한다면 타이머를 취소시킵니다.
+    if (timer) {
+      clearTimeout(timer);
+    }
+
+    // 타이머를 새로 만듭니다.
+    timer = setTimeout(() => func(...args), delay);
+  };
+}
+searchData();
