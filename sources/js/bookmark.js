46205913c6925a5c1120ac190f9c776324bb7a38
UrePu <darkin31313@gmail.com>
2025-01-16 12:23:40 +0900

    a

--- sources/js/bookmark.js
+++ sources/js/bookmark.js
@@ -1,12 +1,17 @@
 function bookMarkBtn(targetTagId, data) {
   let modalBookmarkBtn = document.querySelector(".modal_bookmark_btn");
   const value = JSON.parse(localStorage.getItem("bookMarkedID"));
-  console.log(value);
+  // console.log(value);
   let inputValue = [targetTagId];
   if (value === null) {
     modalBookmarkBtn.innerHTML = "♥";
     localStorage.setItem("bookMarkedID", JSON.stringify(inputValue));
   } else if (value.includes(targetTagId)) {
+    let word = document.querySelector(".main_text").innerHTML;
+    if (word === "북마크 보기") {
+      window.location.reload();
+    }
+
     modalBookmarkBtn.innerHTML = "♡";
     const newArray = value.filter((item) => item !== targetTagId);
     localStorage.setItem("bookMarkedID", JSON.stringify(newArray));
--- sources/js/bookmarkedlist.js
+++ sources/js/bookmarkedlist.js
@@ -1,20 +1,23 @@
 import { getDataFromApi, pageConstruct } from "./script.js";
 
-async function bookMarkedList(params) {
+async function bookMarkedList(val) {
   let word = document.querySelector(".main_text");
+
+  async function bookMarkCreate() {
+    let arr;
+    word.innerHTML = "북마크 보기";
+    const value = JSON.parse(localStorage.getItem("bookMarkedID"));
+    if (value !== null) {
+      arr = await Promise.all(
+        value.map((e) => getDataFromApi("bookmarkData", e))
+      );
+    }
+    pageConstruct({ results: arr }, true);
+  }
+
   document
     .querySelector(".header_bookmark")
-    .addEventListener("click", async () => {
-      word.innerHTML = "북마크 보기";
-      const value = JSON.parse(localStorage.getItem("bookMarkedID"));
-      let arr;
-      if (value !== null) {
-        arr = await Promise.all(
-          value.map((e) => getDataFromApi("bookmarkData", e))
-        );
-      }
-      pageConstruct({ results: arr }, true);
-    });
+    .addEventListener("click", bookmarkcreate);
   document
     .querySelector(".header_bookmark_main")
     .addEventListener("click", () => {
